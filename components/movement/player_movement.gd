ed3d1e2ebe2d52a3f020e2d971dfbb9f4a22c1e0
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2023-11-21 16:05:22 +0000

    refactor velocity setting

--- components/movement/player_movement.gd
+++ components/movement/player_movement.gd
@@ -10,7 +10,9 @@ func _process(delta: float) -> void:
 	var input = Input.get_vector("left", "right", "forward", "backward")
 	var relative_input = Vector2(input.x, input.y).rotated(-player.global_rotation.y)
 	if input.length() > 0:
-		player.velocity = lerp(player.velocity, Vector3(relative_input.x, 0, relative_input.y) * movement_values.max_speed, movement_values.acceleration * delta)
+		#player.velocity = lerp(player.velocity, Vector3(relative_input.x, 0, relative_input.y) * movement_values.max_speed, movement_values.acceleration * delta)
+		player.velocity.x = lerp(player.velocity.x, relative_input.x * movement_values.max_speed, movement_values.acceleration * delta)
+		player.velocity.z = lerp(player.velocity.z, relative_input.y * movement_values.max_speed, movement_values.acceleration * delta)
 	else:
 		player.velocity.x = lerp(player.velocity.x, 0.0, movement_values.friction * delta)
 		player.velocity.z = lerp(player.velocity.z, 0.0, movement_values.friction * delta)
